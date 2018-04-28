e98e3fc8e54daf90859692c082845a49fdf41997
Julian <julian.brodiegold@gmail.com>
2018-04-28 17:48:17 +0100

    Added missing audio files.

---
+++ Assets/Scripts/UIAudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace VoidWars {
+    public class UIAudioPlayer : MonoBehaviour {
+        [SerializeField] private AudioClip _buttonClick;
+        [SerializeField] private AudioClip _errorSound;
+        private AudioSource _audioSource;
+
+        // Use this for initialization
+        private void Start() {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayButtonClick() {
+            _audioSource.PlayOneShot(_buttonClick);
+        }
+
+        public void PlayErrorSound() {
+            _audioSource.PlayOneShot(_errorSound);
+        }
+    }
+}
